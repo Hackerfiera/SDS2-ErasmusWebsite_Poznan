f1fbfecf9f0fe2089a4ade997e4768a0f16f4b17
marta <martagrimaldoslopez@gmail.com>
2023-11-08 22:07:00 +0100

    Degrees

---
+++ Models/Degree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ErasmusSDS.Models
+{
+    public class Degree
+    {
+        [Key]
+        public int DegreeID { get; set; }
+        public string Name { get; set; }
+    }
+}
